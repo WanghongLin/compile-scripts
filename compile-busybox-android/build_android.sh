1c05a05bef4e8c9d352205d48eba712d06d81897
WanghongLin <wh.lin63@gmail.com>
2016-11-25 08:43:52 +0800

    add busybox as submodule

--- compile-busybox-android/build_android.sh
+++ compile-busybox-android/build_android.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Helper script for build busybox for android
+# 
+# Copyright 2016 Wanghong Lin
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# 	http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 VERBOSE="&>/dev/null"
 
 case $OSTYPE in
@@ -11,6 +27,23 @@ case $OSTYPE in
 	*) echo "uknown system"
 		;;
 esac
+
+which 'git' &>/dev/null || {
+    printf '\e[31mgit not installed.\e[30m\n'
+	exit 1
+}
+
+function check_busybox_source()
+{
+	[ -d busybox ] && {
+	    git pull --recurse-submodules
+		return
+	}
+	printf '\e[32mcheck busybox source from official repository\e[30m\n'
+	git submodule add 'https://git.busybox.net/busybox' busybox
+}
+
+check_busybox_source
 
 if [ -z $ANDROID_NDK_ROOT ];then
 	echo "ANDROID_NDK_ROOT not set"
@@ -26,7 +59,7 @@ function clean()
 
 function prepare_config()
 {
-	cp -vf custom/$1 configs/
+	cp -vf ../$1 configs/
 	eval make $1 $VERBOSE
 	if [ "$OSTYPE" == "darwin" ];then
 		sed -i '' 's#ANDROID_NDK_ROOT#'"${ANDROID_NDK_ROOT}"'#' .config
@@ -36,18 +69,20 @@ function prepare_config()
 	rm -v configs/$1
 }
 
+cd busybox
+
 clean
 echo "Build for armeabi with android-9"
 prepare_config android_ndk_r10e_android_9_armeabi_defconfig
 eval make -j8 $VERBOSE
-mkdir -p custom/armeabi
-cp -vf busybox custom/armeabi/
+mkdir -p ../out/armeabi
+cp -vf busybox ../out/armeabi/
 
 clean
 echo "Build for arm64-v8a with android-21"
 prepare_config android_ndk_r10e_android_21_arm64-v8a_defconfig
 eval make -j8 $VERBOSE
-mkdir -p custom/arm64-v8a
-cp -vf busybox custom/arm64-v8a/
+mkdir -p ../out/arm64-v8a
+cp -vf busybox ../out/arm64-v8a/
 
 exit 0
